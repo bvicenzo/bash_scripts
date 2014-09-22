1c3b16b67d981ac60e9e3009e427304ae7fb34e0
Bruno Vicenzo <bruno.nascimento@abril.com.br>
2014-09-22 10:33:43 -0300

    adicionando XVFB (dependência do cucumber)

--- instalacoes/outros_e_dependencias.sh
+++ instalacoes/outros_e_dependencias.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "instalando outras dependencias..."
+
 echo "instalando o Tmux..."
 sudo apt-get install tmux
 
-echo "instalando outras dependencias..."
-
+echo "instalando xvfb para funcionamento correto do Cucumber..."
+sudo apt-get install xvfb -y
