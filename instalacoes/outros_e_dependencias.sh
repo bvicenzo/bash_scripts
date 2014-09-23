11c060c1e368ec1ac29dabbbcd0b436782774090
Bruno Vicenzo <bruno.nascimento@abril.com.br>
2014-09-23 09:56:05 -0300

    adicionando instalação de dependencias do nokogiri

--- instalacoes/outros_e_dependencias.sh
+++ instalacoes/outros_e_dependencias.sh
@@ -7,3 +7,6 @@ sudo apt-get install tmux
 
 echo "instalando xvfb para funcionamento correto do Cucumber..."
 sudo apt-get install xvfb -y
+
+echo "Instalando dependencias do nokogiri..."
+sudo apt-get install libxslt-dev libxml2-dev
